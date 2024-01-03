1b30a2430956a9083876f10c3a5972cef473b8c7
mustafakhafaji <143281566+mustafakhafaji@users.noreply.github.com>
2024-01-02 19:30:24 -0500

    Vector3 -> Vector4

---
+++ Math/Vector/vector.h
@@ -0,0 +1,22 @@
+typedef struct Vector Vector;
+
+// Returns sum of 2 vectors.
+Vector* Vector_Add(const Vector* a, const Vector* b);
+
+// Returns product of 2 vectors.
+Vector* Vector_Multiply(const Vector* a, const Vector* b);
+
+// Returns result of a / b.
+Vector* Vector_Divide(const Vector* a, const Vector* b);
+
+// Returns 1 if given vectors are equal.
+int Vector_isEqual(const Vector* a, const Vector* b);
+
+// Returns length of a vector.
+float Vector_Length(const Vector* a);
+
+// Returns dot product of given vectors.
+float Vector_Dot(const Vector* a, const Vector* b);
+
+// Returns cross product of given vectors.
+Vector* Vector_Cross(const Vector* a, const Vector* b);
